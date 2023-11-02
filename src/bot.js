549bd739e2d85b8dc5cca164e7852ce44a9681ea
blushell <micheal.jones89@gmail.com>
2023-11-02 05:13:09 -0400

    setup

---
+++ src/bot.js
@@ -0,0 +1,7 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const request = require('request')
+const Gamedig = require('gamedig');
+const boxen = require('boxen');
+
+const fs = require('fs');
+const path = require('path');
