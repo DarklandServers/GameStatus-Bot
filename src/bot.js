a28a7503b9ed5554bca85147ef088bdcd6af5927
blushell <micheal.jones89@gmail.com>
2023-11-02 07:43:19 -0400

    config handle & reader

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,27 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const request = require('request')
 const Gamedig = require('gamedig');
 const boxen = require('boxen');
+const { createDefaultConfig, getConfig } = require('./configHandler');
 
-const fs = require('fs');
-const path = require('path');
+async function startCheck() {
+    try {
+      if (config) {
+        const config = await getConfig();
+        const runningServers = config.SERVERS.filter(server => server.RUNNING);
+
+        if (runningServers.length > config.maxServers) {
+          console.log("Max servers is over " + config.maxServers)
+          console.log("Please verify max servers and try again")
+          process.exit()
+        }
+        startBots();
+      }
+    } catch (err) {
+      await createDefaultConfig();
+      process.exit()
+    }
+}
+
+
+
+startCheck();
