509d30b4c143d1bfc279ee79f369e38e7fe90de5
blushell <micheal.jones89@gmail.com>
2023-11-11 10:22:20 -0500

    minor fixes

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,4 @@
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
-const DiscordRPC = require('discord-rpc');
-const RPC = new DiscordRPC.Client({ transport: 'ipc' });
 const request = require('request')
 const Gamedig = require('gamedig')
 const fs = require('fs');
@@ -8,124 +6,108 @@ const path = require('path');
 const createDefaultConfig = require('./configHandler');
 const startCheck = require('./startCheck');
 
-    createDefaultConfig();
-    startCheck();
+createDefaultConfig();
+startCheck();
 
-    try {
+try {
 
-        const configData = JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'config.json'), 'utf8'));
-        const runEnabled = configData.SERVERS.filter(server => server.RUNNING);
-        const updateInterval = (1000 * 60) * configData.updateInterval;
+    const configData = JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'config.json'), 'utf8'));
+    const runEnabled = configData.SERVERS.filter(server => server.RUNNING);
+    const updateInterval = (1000 * 60) * configData.updateInterval;
 
-        for(var i = 0; i < runEnabled.length; i++) { 
-
-            var config = runEnabled[i];
-            const { botAppId, botUserId, rpcEnable, botToken, apiType, apiUrl, serverIp, serverPort, queueMessage, showMap, mapPrefix, gameType, debug } = config;
-
-            function updateActivity() {
-                return new Promise((resolve) => {
-                    if (apiType === 1) {
-                        require('tls').DEFAULT_ECDH_CURVE = 'auto'
-                        request({ url: apiUrl, headers: { json: true}, timeout: 10000 }, function (err, res, body) {
-                            if (!err && res.statusCode == 200) {
-                                const jsonData = JSON.parse(body);
-                                const server = jsonData.data.attributes;
-                                const is_online = server.status;
-                                if (is_online == 'online') {
-                                    const status = 'online';
-                                    const players = server.players;
-                                    const maxplayers = server.maxPlayer;
-                                    /* const queue = server.details.rust_queued_players;
-                                    if (typeof queue !== 'undefined' && queue != '0') {
-                                        statusTitle += ` (${queue} ${queueMessage})`;
-                                    } */
-                                    if (debug) console.log('Updated from Battlemetrics, serverID: ' + server.id);
-                                    //RETURN ONLINE
-                                    resolve({ players, maxplayers, mapName: null, status });
-                                } else {
-                                    const status = 'idle'
-                                    const statusTitle = 'Offline'
-                                    resolve({ statusTitle, status });
-                                } 
+    for(var i = 0; i < runEnabled.length; i++) { 
+        function updateActivity() {
+            if (apiType === 1) {
+                require('tls').DEFAULT_ECDH_CURVE = 'auto'
+                request({ url: apiUrl, headers: { json: true}, timeout: 10000 }, function (err, res, body) {
+                    if (!err && res.statusCode == 200) {
+                        const jsonData = JSON.parse(body);
+                        const server = jsonData.data.attributes;
+                        const is_online = server.status;
+                        if (is_online == 'online') {
+                            const players = server.players;
+                            const maxplayers = server.maxPlayer;
+                            const queue = server.details.rust_queued_players
+                            let status = `${players}/${maxplayers}`
+                            if (typeof queue !== "undefined" && queue != "0") {
+                                status += ` (${queue} ${queueMessage})`
                             }
-                        });
-                    } else if (apiType === 2) {
-                        if (!serverIp || !serverPort) {
-                            console.log('You have to configure server IP/Port')
-                            process.exit();
+                            if (debug) console.log('Updated from Battlemetrics, serverID: ' + server.id);
+                            //RETURN ONLINE
+                            return client.user.setPresence({ activities: [{ name: `${configData.statusPrefix} ${status}`, type: ActivityType.Custom}], status: 'online'});
                         } else {
-                            Gamedig.query({
-                                type: gameType,
-                                host: serverIp,
-                                port: serverPort
-                            }).then((state) => {
-                                if (debug) { console.log(state); }
-                                const status = 'online'
-                                const players = state.raw.numplayers;
-                                const maxplayers = state.maxplayers;
-                                const mapData = state.map;
-                                const mapName = mapData.replace(new RegExp(`^(${mapPrefix.join('|')})_`, 'i'),'').replace(/_/g, ' ');
-                                //RETURN ONLINE
-                                resolve({ players, maxplayers, mapName, status });
-                            }).catch(() => {
-                                const status = 'idle'
-                                const statusTitle = 'Offline'
-                                resolve({ statusTitle, status });
-                            });                    
-                        }
-                    } else {
-                        console.log('invalid API type');
-                        process.exit();                     
+                            //RETURN OFFLINE
+                            return client.user.setPresence({ activities: [{ name: 'Offline', type: ActivityType.Custom}], status: 'idle'});
+                        } 
                     }
                 });
-            } /* STAT UPDATE END */
-
-            async function statusUpdate() {
-                try {
-                    const { players, maxplayers, mapName, status } = await updateActivity();
-                    const statusTitle = showMap ? `${players}/${maxplayers} ${configData.statusSpacer} ${mapName}` : `${players}/${maxplayers}`;                               
-                    client.user.setPresence({ activities: [{ name: `${configData.statusPrefix} ${statusTitle}`, type: ActivityType.Custom}], status: `${status}`});
-
-                    if (!RPC) return
-
-                    if (RPC.user.id === botUserId) {
-                        RPC.setActivity({
-                            details: `${players}/${maxplayers}`,
-                            state: `${mapName}`,
-
-                        })
-                    }
-
-                } catch (err) { console.error('An error occurred:', err); }
+            } else if (apiType === 2) {
+                if (!serverIp || !serverPort) {
+                    console.log('You have to configure server IP/Port')
+                    process.exit();
+                } else {
+                    Gamedig.query({
+                        type: gameType,
+                        host: serverIp,
+                        port: serverPort
+                    }).then((state) => {
+                        if (debug) { console.log(state); }
+                        const players = state.raw.numplayers;
+                        const maxplayers = state.maxplayers;
+                        let status = `${players}/${maxplayers}`
+                        const mapData = state.map;
+                        const mapName = mapData.replace(new RegExp(`^(${mapPrefix.join('|')})_`, 'i'),'').replace(/_/g, ' ');
+                        status = showMap ? `${players}/${maxplayers} ${configData.statusSpacer} ${mapName}` : `${players}/${maxplayers}`;
+                        //RETURN ONLINE
+                        return client.user.setPresence({ activities: [{ name: `${configData.statusPrefix} ${status}`, type: ActivityType.Custom}], status: 'online'});
+                    }).catch(() => {
+                        //RETURN OFFLINE
+                        return client.user.setPresence({ activities: [{ name: 'Offline', type: ActivityType.Custom}], status: 'idle'});
+                    });                    
+                }
+            } else {
+                console.log('invalid API type');
+                process.exit();                     
             }
+        } /* STAT UPDATE END */
 
-            const client = new Client({
-                intents: [
-                    GatewayIntentBits.Guilds,
-                    GatewayIntentBits.GuildMessages,
-                    GatewayIntentBits.MessageContent,
-                ]
-            });
+        var config = runEnabled[i];
+        const { botToken, apiType, apiUrl, serverIp, serverPort, queueMessage, showMap, mapPrefix, gameType, debug } = config;
 
-            client.once('ready', () => {
-                statusUpdate();
-                setInterval(statusUpdate, updateInterval);
-            });
-                        
-            client.on('error', err => {
-                if (debug) console.error(error(err))
-                });
-                
-            process.on('unhandledRejection', err => {
-                if (err.code == 'TOKEN_INVALID') 
-                    return console.error('Error: An invalid token was provided.\nYou may have added a client secret instead of a BOT token.\nPlease set the BOT token.');
-                
-                return console.error('Unhandled promise rejection:', err);
-            });
+        const client = new Client({
+            intents: [
+              GatewayIntentBits.Guilds,
+              GatewayIntentBits.GuildMessages,
+              GatewayIntentBits.MessageContent,
+            ]
+        });
 
-            DiscordRPC.register(botAppId);
-            client.login(botToken)
-            RPC.login({ clientId: botAppId });
+        client.on("ready", () => {
+            console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`)
+            updateActivity();
+            setInterval(updateActivity, updateInterval);
+        });
         
-        } /* END OF FOR LOOP */ 
-    } catch (err) { console.error('An error occurred:', err); } /* END OF TRY */
+        client.on("guildCreate", guild => {
+            console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`)
+        })
+            
+        client.on("guildDelete", guild => {
+            console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`)
+        })
+            
+        client.on('error', function (error) {
+            if (debug) console.log(error)
+        })
+    
+        process.on('unhandledRejection', error => {
+            if (error.code == 'TOKEN_INVALID') 
+                return console.log("Error: An invalid token was provided.\nYou have maybe added client secret instead of BOT token.\nPlease set BOT token")
+                
+            return console.error('Unhandled promise rejection:', error);
+        });
+
+        client.login(botToken)
+    
+    } /* END OF FOR LOOP */ 
+} catch (err) { console.error('An error occurred:', err); } /* END OF TRY */
